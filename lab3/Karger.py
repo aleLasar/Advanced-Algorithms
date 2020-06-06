5fd07cd26c8cdb15437326bc3fbb087b8216b8b1
Matteo Marchiori <matteo.marchiori97@gmail.com>
2020-06-06 18:04:26 +0200

    lettura file in grafo, inizio karger, da correggere

--- lab3/Karger.py
+++ lab3/Karger.py
@@ -1,20 +1,18 @@
 import math
 import os
+import random
 import sys
 from threading import Timer
 import time
 
-PI = 3.141592
-RRR = 6378.388
-
 
 class Node():
 
-    def __init__(self, name, latitude, longitude):
+    def __init__(self, name, index):
         self._name = name
+        self._index = index
+        self._adjacents = []
         self._visited = False
-        self._latitude = latitude
-        self._longitude = longitude
 
     def __gt__(self, other):
         return self._name > other._name
@@ -31,11 +29,14 @@ class Node():
     def __eq__(self, other):
         return self._name == other._name
 
+    def __hash__(self):
+        return self._name    
+
     def __ne__(self, other):
         return self._name != other._name
 
     def __repr__(self):
-        return str(self._name+1)
+        return str(self._name)
 
     def name(self):
         return self._name
@@ -43,85 +44,26 @@ class Node():
     def set_name(self, name):
         self._name = name
 
+    def index(self):
+        return self._index
+
+    def set_index(self, index):
+        self._index = index    
+
+    def adjacents(self):
+        return self._adjacents
+
+    def add_adjacent(self, node):
+        self._adjacents.append(node)
+
+    def set_adjacents(self, adjacents):
+        self._adjacents = adjacents
+
     def visited(self):
         return self._visited
 
     def set_visited(self, visited):
         self._visited = visited
-
-    def latitude(self):
-        return self._latitude
-
-    def latitude_rad(self):
-        deg = int(self._latitude)
-        rounded = self._latitude - deg
-        return PI * (deg + 5.0 * (rounded / 3.0)) / 180.0
-
-    def set_latitude(self, latitude):
-        self._latitude = latitude
-
-    def longitude(self):
-        return self._longitude
-
-    def longitude_rad(self):
-        deg = int(self._longitude)
-        rounded = self._longitude - deg
-        return PI * (deg + 5.0 * (rounded / 3.0)) / 180.0
-
-    def set_longitude(self, longitude):
-        self._longitude = longitude
-
-
-class Edge():
-
-    def __init__(self, src, dest, weight):
-        self._nodes = src, dest
-        self._weight = weight
-        self._label = None
-
-    def __gt__(self, other):
-        return self._weight > other._weight
-
-    def __lt__(self, other):
-        return self._weight < other._weight
-
-    def __le__(self, other):
-        return self._weight <= other._weight
-
-    def __ge__(self, other):
-        return self._weight >= other._weight
-
-    def __eq__(self, other):
-        return self._weight == other._weight
-
-    def __ne__(self, other):
-        if other == None:
-            return True
-        else:
-            return self._weight != other._weight
-
-    def __str__(self):
-        return self._weight
-
-    def weight(self):
-        return self._weight
-
-    def nodes(self):
-        return self._nodes
-
-    def label(self):
-        return self._label
-
-    def set_label(self, label):
-        self._label = label
-
-    def opposite(self, node):
-        if node == self._nodes[0]:
-            return self._nodes[1]
-        if node == self._nodes[1]:
-            return self._nodes[0]
-        return None
-
 
 # %%
 
@@ -129,30 +71,22 @@ class Edge():
 class Graph():
 
     def __init__(self, n):
-        self._edge = dict()
         self._nodes = [None] * n
+        self._lastnode = n
 
-    def add_node(self, name, latitude, longitude):
-        if not self.is_node_present(name):
-            node = Node(name, latitude, longitude)
-            self._nodes[name] = node
+    def add_node(self, name, index):
+        if not self.is_node_present(index):
+            node = Node(name, index)
+            self._nodes[index] = node
             return node
         else:
-            return self._nodes[name]
-
-    def add_edge(self, src, dest, weight):
-        edge = Edge(src, dest, weight)
-        self._edge[src.name(), dest.name()] = edge
-        self._edge[dest.name(), src.name()] = edge
-
-    def edges(self):
-        return self._edge
+            return self._nodes[index]
 
     def nodes(self):
         return self._nodes
 
-    def is_node_present(self, name):
-        if self._nodes[name] is None:
+    def is_node_present(self, index):
+        if self._nodes[index] is None:
             return False
         else:
             return True
@@ -160,105 +94,54 @@ class Graph():
     def num_vertici(self):
         return len(self._nodes)
 
-    def remove_edge(self, edge):
-        self._edge[edge.nodes()[0].name()][edge.nodes()[1].name()] = None
-        self._edge[edge.nodes()[1].name()][edge.nodes()[0].name()] = None
+    def inc_lastnode(self):
+        self._lastnode += 1
+        return self._lastnode
 
-    def geo_distance(self, src, dest):
-        q1 = math.cos(src.longitude_rad() - dest.longitude_rad())
-        q2 = math.cos(src.latitude_rad() - dest.latitude_rad())
-        q3 = math.cos(src.latitude_rad() + dest.latitude_rad())
-        return int(RRR * math.acos(0.5*((1.0+q1)*q2 - (1.0-q1)*q3)) + 1.0)
 
-    def euclide_distance(self, src, dest):
-        x = abs(src.latitude() - dest.latitude())
-        y = abs(src.longitude() - dest.longitude())
-        return int(math.sqrt(math.pow(x, 2)+math.pow(y, 2)))
-
-def cheapest_insertion(G:Graph):
-    edges = G.edges()
-    nodes = list(G.nodes())
-    edges_sol = {}
-    weight_sol = 0
-    zero_n = nodes.pop(0)
-    
-    while len(nodes) != 0:
-        local_min = float("inf")
-        local_edge = [None]*3
-        local_node = None
-        if len(edges_sol) == 0:
-            for i in range(len(nodes)):
-                val = edges[zero_n.name(), nodes[i].name()]
-                if val.weight() < local_min:
-                    local_min = val.weight()
-                    local_node = nodes[i]
-                    local_edge[0] = val
-        else:
-            for k in range(len(nodes)):
-                for idx, val in enumerate(edges_sol.values()):
-                    ik = edges[nodes[k].name(), val.nodes()[0].name()]
-                    jk = edges[nodes[k].name(), val.nodes()[1].name()]
-                    ij = val
-
-                    to_minimized = ik.weight() + jk.weight() - ij.weight()
-                    if to_minimized < local_min:
-                        local_min = to_minimized
-                        local_node = nodes[k]
-                        local_edge = ik, jk, val
-
-        nodes.remove(local_node)
-        if not local_edge[1]:
-            weight_sol += local_min*2
-            nodes_e = local_edge[0].nodes()
-            edges_sol[nodes_e[0].name(), nodes_e[1].name()] = local_edge[0]
-            edges_sol[nodes_e[1].name(), nodes_e[0].name()] = Edge(nodes_e[1], nodes_e[0], local_edge[0].weight())
-        else:
-            weight_sol += local_edge[0].weight() + local_edge[1].weight() - local_edge[2].weight()
-            nodes_0 = local_edge[0].nodes()
-            nodes_1 = local_edge[1].nodes()
-            nodes_2 = local_edge[2].nodes()
-            edges_sol[nodes_0[0].name(), nodes_0[1].name()] = local_edge[0]
-            edges_sol[nodes_1[0].name(), nodes_1[1].name()] = local_edge[1]
-            del edges_sol[nodes_2[0].name(), nodes_2[1].name()]
-
-    return weight_sol
+def karger(graph):
+    nodes = graph.nodes()
+    print(nodes)
+    src_index = random.randint(0,len(nodes)-1)
+    src = nodes[src_index]
+    print(src)
+    print(src.adjacents())
+    for i in range(len(nodes)-2):
+        adjacents_src = src.adjacents()
+        dest = random.choice(adjacents_src)
+        dest_index = dest.index()
+        print(dest)
+        print(dest.adjacents())
+        adjacents_dest = dest.adjacents()
+        newname = graph.inc_lastnode()
+        newadjacents = list(set(
+            adjacents_src + list(set(adjacents_dest)-set(adjacents_src))) - set((src, dest)))
+        print(newadjacents)    
+        newnode = Node(newname, src.index()) 
+        newnode.set_adjacents(newadjacents)   
+        for node in newadjacents:
+            for i,adjacent in enumerate(node.adjacents()):
+                if adjacent is src:
+                    node.adjacents()[i] = newnode
+        nodes[src_index] = None
+        nodes[dest_index] = newnode
+        src = random.choice(newadjacents)
+        src_index = src.index()
 
 
 def read_file(filename):
     file = open(filename, "r")
-    type = ""
-    vertici = 0
-
-    while True:
-        line = file.readline()
-        line = line.replace(" :", ":")
-        if "NODE_COORD_SECTION" in line:
-            break
-        label, value = list(line.split(maxsplit=1))
-        if "EDGE_WEIGHT_TYPE" in label:
-            type = value
-        if "DIMENSION" in label:
-            vertici = int(value)
-
-    graph = Graph(vertici)
-
+    graph = Graph(len(file.readlines()))
+    file.seek(0)
     for line in file:
-        if "EOF" in line:
-            break
-        if "GEO" in type:
-            tripla = list(map(float, line.split()))
-        else:
-            tripla = list(map(int, map(round, map(float, line.split()))))
-        graph.add_node(int(tripla[0])-1, tripla[1], tripla[2])
-    nodes = graph.nodes()
-    for index_src in range(len(nodes)):
-        for index_dest in range(index_src+1, len(nodes)):
-            src = nodes[index_src]
-            dest = nodes[index_dest]
-            if "GEO" in type:
-                graph.add_edge(src, dest, graph.geo_distance(src, dest))
+        nodi = line.split()
+        for i, name in enumerate(nodi):
+            intname = int(name)
+            if i is 0:
+                root = graph.add_node(intname, intname-1)
             else:
-                graph.add_edge(src, dest, graph.euclide_distance(src, dest))
+                node = graph.add_node(intname, intname-1)
+                root.add_adjacent(node)
     file.close()
     return graph
 
@@ -268,18 +151,19 @@ def errore(soluzione, ottimo):
 
 
 def main(folder):
-    ottimi_file = open(folder+"/"+"ottimi.txt", "r")
-    ottimi = {}
-    for line in ottimi_file:
-        graph, ottimo = list(line.split(":"))
-        ottimi[graph] = int(ottimo)
-    ottimi_file.close()
     with os.scandir(folder) as it:
         for i, entry in enumerate(it):
-            if "tsp" in entry.name:
-                ottimo = ottimi[entry.name]
+            if "input_random_1_6" in entry.name:
                 graph = read_file(folder+"/"+entry.name)
-                print(entry.name)
+                karger(graph)
+                nodes = graph.nodes()
+                for node in nodes:
+                    if node is not None:
+                        print(node)
+                        for adjacent in node.adjacents():
+                            if adjacent is not None:
+                                print("  "+str(adjacent))
+                """print(entry.name)
                 start = time.time()
                 dist = cheapest_insertion(graph)
                 time_exec = time.time() - start
@@ -287,9 +171,8 @@ def main(folder):
                 result = open(folder+"/"+test, "a")
                 result.write("\nCheapest_Insertion - soluzione: " +
                              str(dist)+" tempo: "+str(time_exec) + " errore: "+errore(dist, ottimo)+" %")
-                result.close()
+                result.close()"""
 
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(999999999)
-    main("tsp_dataset")
+    main("mincut_dataset")
